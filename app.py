d80677d9ca5546863b9af8b9ccf6970a4bb41949
guerriers <faiycoc1508@gmail.com>
2023-11-13 22:48:23 +0700

    fix menu

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-from streamlit_option_menu import option_menu
 
 # Create a sidebar for the menu
 menu = st.sidebar.selectbox("Menu", ["salmon", "Bar Chart", "Scatter Chart"])
