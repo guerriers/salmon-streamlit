4536294af273358d933aa1310a51e7f999fe9098
guerriers <faiycoc1508@gmail.com>
2023-11-13 22:07:23 +0700

    db fix

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
+import os
+
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 
 st.header("salmon")
 
